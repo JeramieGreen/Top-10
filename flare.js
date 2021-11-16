25c3cbae54061a440b1e3fb9a6811f4875ad9771
Jeramie Green <jeramiegreen87@gmail.com>
2021-11-15 21:53:38 -0800

    giving action to my home page js file

---
+++ flare.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let userName = prompt("What is your name?");
+
+let currentHour = prompt("Who had the number one spot 'Locked-Down' on your playlist in 2020?"); // 11
+let greeting = "";
+
+
+
+console.log(userName);
+
+
+document.write(greeting + "Ready for This List, " + userName.toUpperCase() + "?!");
