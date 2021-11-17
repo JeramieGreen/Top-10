8ee1ac33fa44735f1e5fb51bd63b2e94408cfeac
Jeramie Green <jeramiegreen87@gmail.com>
2021-11-16 19:14:13 -0800

    committing the page prompts to talk with visitors

--- flare.js
+++ flare.js
@@ -1,13 +1,36 @@
 "use strict"
 
-let userName = prompt("What is your name?");
+alert("Click through to see who got the top spot!");
 
-let currentHour = prompt("Who had the number one spot 'Locked-Down' on your playlist in 2020?"); // 11
-let greeting = "";
+let userName = prompt("Hi! What do you call yourself?");
 
+let currentHour = prompt("Who had the number one spot 'Locked-Down' on your playlist in 2020?");
+
+//function hourOfDay(){
+   // let currentHour = prompt("Who had the number one spot 'Locked-Down' on your playlist in 2020?");
+  //  let greeting = "";
+   // if(currentHour = "The Weeknd" || currentHour = "the weeknd"){
+  //      greeting = "You gotta see this list!"; 
+  //  } else if(currentHour = "The weeknd" || currentHour = "weeknd"){
+  //      greeting = "You gotta see this list!";
+  //  } else if(currentHour > 0 ) {
+  //      greeting = "Good Morning";
+  //  }else {
+  //      greeting = "Welcome";
+    //    return greeting;
+//
+  //  }
+    //return greeting;
+
+
+// 11
+
+
+//let greeting = "Click through to see got the top spot!";
 
 
 console.log(userName);
+
 
 
 document.write(greeting + "Ready for This List, " + userName.toUpperCase() + "?!");
